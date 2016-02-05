d79dff9ceb1078e4b1fd96b89e0a2499b3941e93
gnrockone <gnrockone@gmail.com>
2016-02-05 20:48:50 +0800

    updated style.scss realest.js

--- wp-content/themes/realest/js/realest.js
+++ wp-content/themes/realest/js/realest.js
@@ -1,4 +1,29 @@
 /*! Please do not remove ! for uglify comment preservation
  */
 
+jQuery(document).ready( function($) {
+	
+
+
+
+
+/*
+==================================================
+| jquery sticky back to top button
+==================================================
+*/
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > 200) {
+			$('.go-top').fadeIn(200);
+		} else {
+			$('.go-top').fadeOut(200);
+		}
+	});	
+	// Animate the scroll to top
+	$('.go-top').click(function(event) {
+		event.preventDefault();
+		$('html, body').animate({scrollTop: 0}, 300);
+	})
+
+});
 
