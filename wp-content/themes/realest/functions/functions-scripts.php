50ad23fd9dbc29dd2187b7f45ed4e80533df6d0b
gnrockone <gnrockone@gmail.com>
2016-02-10 22:16:45 +0800

    update function scripts

--- wp-content/themes/realest/functions/functions-scripts.php
+++ wp-content/themes/realest/functions/functions-scripts.php
@@ -10,16 +10,18 @@
 // if (!function_exists('nnp_enqueque_scripts')) {
 // 	function nnp_enqueue_scripts() {
 // 		//css
+//      wp_enqueue_style('normalize', get_template_directory_uri() . '/bower_components/normalize-css.css', array(), '1.0.0.0', 'all');
 // 		wp_enqueue_style('wordpress-default', get_template_directory_uri() . '/css/wordpress-default.css', array(), '1.0.0.0', 'all');
-// 		wp_enqueue_style('bootstrap',get_template_directory_uri(). '/css/bootstrap.css',array(),'3.3.5','all');
+// 		wp_enqueue_style('bootstrap',get_template_directory_uri(). '/bower_components/bootstrap/dist/css/bootstrap.css',array(),'3.3.5','all');
 // 		wp_enqueue_style('style',get_stylesheet_uri(),array(),'1.0.0','all');
-// 		wp_enqueue_style('custom',get_template_directory_uri() .'/css/custom.css',array(),'1.0.0','all');
+// 		wp_enqueue_style('all',get_template_directory_uri() .'/css/all.css',array(),'1.0.0','all');
 // 		//js
 // 		wp_enqueue_script('jquery');
-// 		wp_enqueue_script('bootstrapjs',get_template_directory_uri(). '/js/bootstrap.js',array('jquery'),'3.3.5',true);
+// 		wp_enqueue_script('modernizr',get_template_directory_uri(). '/bower_components/modernizr/node_modules/phantomjs/lib/phantom/examples/modernizr.js',array('jquery'),'3.3.5',true);
+// 		wp_enqueue_script('bootstrapjs',get_template_directory_uri(). '/bower_components/bootstrap/dist/js/bootstrap.js',array('jquery'),'3.3.5',true);
 // 		wp_enqueue_script('masonryjs',get_template_directory_uri(). '/js/masonry.pkgd.js',array('jquery'), '',true);
 // 		wp_enqueue_script('imagesloadedjs',get_template_directory_uri(). '/js/imagesloaded.pkgd.js',array('jquery'),'',true);
-// 		wp_enqueue_script('customjs',get_template_directory_uri(). '/js/customjs.js',array(),'1.0.0',true);
+// 		wp_enqueue_script('realestjs',get_template_directory_uri(). '/js/realest.js',array(),'1.0.0',true);
 // 	}
 // }
 // add_action('wp_enqueue_scripts','nnp_enqueue_scripts');
